14b4c21e548a8dff69970ca7f8b0844bf0c8edc7
ejin0517 <91935021+ejin0517@users.noreply.github.com>
2021-10-26 12:51:11 +0900

    Add files via upload

---
+++ doc/slick02.js
@@ -0,0 +1,22 @@
+$(function(){
+    //-----------------------
+
+    //메인슬라이드
+$('.main_slider').slick({
+    arrows:false, //버튼이 없어짐//
+    autoplay:true,
+    pauseOnHover:false,
+});
+
+
+    //제품슬라이드
+$('.pr_slider').slick({
+    arrows:false,
+    autoplay:true,
+    slidesToShow:3, //몇개씩 보이도록 하는지//
+    dots:true, //밑에 점이 나오도록//
+});
+
+
+    //-----------------------------
+})
