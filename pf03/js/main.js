615c5095574832282df822e42fbefa89e574ab1b
ejin0517 <91935021+ejin0517@users.noreply.github.com>
2021-11-17 14:53:22 +0900

    Add files via upload

---
+++ pf03/js/main.js
@@ -0,0 +1,41 @@
+$(function (){
+
+
+    $('#main_visual').slick({
+        arrows:false,
+        autoplay:true,
+        pauseOnHover:false,
+        pauseOnFocus:false,
+        speed:2000,
+        dots:true,
+    });
+
+
+
+$('.tabmenu ul>li').on('click', function(){
+
+    var idx= $(this).index();
+    //console.log(idx);
+    $(this).addClass('on').siblings().removeClass('on');
+    $('.tab_content_menu').eq(idx).addClass('on').siblings().removeClass('on');
+    
+});
+
+
+$('.news_slider').slick({
+    arrows:false,
+    autoplay:true,
+    speed:2000,
+    slidesToShow:3,
+});
+
+
+$('#content05 .xi-angle-left-min').on('click',function(){
+    $('.news_slider').slick('slickPrev');
+});
+$('#content05 .xi-angle-right-min').on('click',function(){
+    $('.news_slider').slick('slickNext');
+});
+
+
+})
