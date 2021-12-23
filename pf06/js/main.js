b603663de5398ee8511061f948260e5d91a9f8a2
ejin0517 <91935021+ejin0517@users.noreply.github.com>
2021-12-23 21:18:18 +0900

    Add files via upload

--- pf06/js/main.js
+++ pf06/js/main.js
@@ -43,24 +43,52 @@ $('.main_bn_slider').slick({
     pauseOnHover:false,
 });
 
+
 $(".mv01").YTPlayer({
     videoURL:'https://youtu.be/v7j2BMefsm8',
-    containment:'.mv01',
-    autoPlay:true, 
+    containment:'.mv01 figure',
+    autoPlay:true,
     mute:true, 
+    abundance:0,
     startAt:0, 
     opacity:1,
     showControls:false, //control이 안보이게, 보이게하려면 true
 });
 
+$('.video01 i.xi-pause').on('click', function(){
+    $(".mv01").YTPPause();
+});
+
+$('.video01 i.xi-play').on('click', function(){
+    $(".mv01").YTPPlay();
+});
+
+$('.video01 i.xi-expand-square').on('click', function(){
+    $(".mv01").YTPFullscreen();
+});
+
+
 $(".mv02").YTPlayer({
     videoURL:'https://youtu.be/DzVmzMf7C_U',
-    containment:'.mv02',
-    autoPlay:true, 
+    containment:'.mv02 figure',
+    autoPlay:true,
     mute:true, 
     startAt:0, 
+    abundance:0,
     opacity:1,
     showControls:false, //control이 안보이게, 보이게하려면 true
+});
+
+$('.video02 i.xi-pause').on('click', function(){
+    $(".mv02").YTPPause();
+});
+
+$('.video02 i.xi-play').on('click', function(){
+    $(".mv02").YTPPlay();
+});
+
+$('.video02 i.xi-expand-square').on('click', function(){
+    $(".mv02").YTPFullscreen();
 });
 
 //-------------------------
