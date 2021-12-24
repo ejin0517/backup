b66374a84f380c832f28d8468f355de7badd35e8
ejin0517 <91935021+ejin0517@users.noreply.github.com>
2021-12-24 15:13:33 +0900

    Add files via upload

--- pf06/js/main.js
+++ pf06/js/main.js
@@ -43,55 +43,6 @@ $('.main_bn_slider').slick({
     pauseOnHover:false,
 });
 
-/*
-$(".mv01").YTPlayer({
-    videoURL:'https://youtu.be/v7j2BMefsm8',
-    containment:'.mv01 figure',
-    autoPlay:true,
-    mute:true, 
-    abundance:0,
-    startAt:0, 
-    opacity:1,
-    showControls:false, //control이 안보이게, 보이게하려면 true
-});
-
-$('.video01 i.xi-pause').on('click', function(){
-    $(".mv01").YTPPause();
-});
-
-$('.video01 i.xi-play').on('click', function(){
-    $(".mv01").YTPPlay();
-});
-
-$('.video01 i.xi-expand-square').on('click', function(){
-    $(".mv01").YTPFullscreen();
-});
-
-
-$(".mv02").YTPlayer({
-    videoURL:'https://youtu.be/DzVmzMf7C_U',
-    containment:'.mv02 figure',
-    autoPlay:true,
-    mute:true, 
-    startAt:0, 
-    abundance:0,
-    opacity:1,
-    showControls:false, //control이 안보이게, 보이게하려면 true
-});
-
-$('.video02 i.xi-pause').on('click', function(){
-    $(".mv02").YTPPause();
-});
-
-$('.video02 i.xi-play').on('click', function(){
-    $(".mv02").YTPPlay();
-});
-
-$('.video02 i.xi-expand-square').on('click', function(){
-    $(".mv02").YTPFullscreen();
-});
-*/
-
 
 $(window).on("scroll", function(){
     if ($(window).scrollTop() > 100) {
@@ -109,7 +60,7 @@ $('.gotop').on('click', function(){
 //-------------------------
 
 
-$('.mopen').on('click',function(){
+$('.m_open i').on('click',function(){
     $('.gnb').toggleClass('on');
 });
 
@@ -123,7 +74,15 @@ $('.gnb>ul>li>a').on('click', function(){
     
 });
 
-
+//---------------------------------
+AOS.init({
+    disable: 'mobile',
+    disable: function() {
+      var maxWidth = 768;
+      return window.innerWidth < maxWidth;
+    }
+    
+  });
 
 //--------------------------
 })
