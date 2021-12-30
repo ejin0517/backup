e40d5d69713f18522674dfb284baebe84d616909
ejin0517 <91935021+ejin0517@users.noreply.github.com>
2021-12-30 19:03:13 +0900

    Add files via upload

---
+++ pf07/js/main.js
@@ -0,0 +1,50 @@
+$(function(){
+//-----------------------
+
+$('#header .search').hide();
+$('#header .search_icon').on('click', function(){
+    $('#header .search').slideToggle();
+});
+
+$('.search .xi-close').on('click', function(){
+    $('#header .search').slideUp();
+})
+
+$('.main_visual').slick({
+        arrows:false,
+        autoplay:true,
+        fade:true,
+        autoplaySpeed:4000,
+        pauseOnHover:false,
+        pauseOnFocus:false,
+});
+
+$('.arrow i.xi-angle-left-min').on('click', function(){
+    $('.main_visual').slick('slickPrev');
+});
+$('.arrow i.xi-angle-right-min').on('click', function(){
+    $('.main_visual').slick('slickNext');
+});
+
+$('.slider .news_slider').slick({
+    arrows:false,
+    autoplay:true,
+    pauseOnHover:false,
+    pauseOnFocus:false,
+    slidesToShow:3,
+    centerMode:true,
+});
+
+$('.news_slider figure').eq(4).addClass('on');
+$('.news_slider').on('afterChange',function(e,s,c){
+    $('.news_slider figure').eq(c+4).addClass('on').siblings().removeClass('on');
+});
+
+
+$( "#datepicker" ).datepicker();
+
+
+
+
+//--------------------------
+})
