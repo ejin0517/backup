09fb98e838c5ffe4cc318e6d9afeedc4e036af03
ejin0517 <91935021+ejin0517@users.noreply.github.com>
2021-12-31 17:14:04 +0900

    Add files via upload

--- trn04/js/layout.js
+++ trn04/js/layout.js
@@ -1,9 +1,25 @@
 $(function(){
 //--------------------
 
+$('#popup').draggable();
+$('#popup a').on('click', function(){
+    $(this).parent().fadeOut();
+    return false();
+})
+
 $(window).on('scroll', ()=>{
     let sct=$(window).scrollTop();
     sct > 0 ? $('#header').addClass('on') : $('#header').removeClass('on');
+    sct > 300 ? $('#totop').fadeIn() : $('#totop').fadeOut();
+    sct > sTop ? $('#solution').addClass('on') : $('#solution').removeClass('on');
+});
+
+$('.search').on('click', function(){
+    $('.searchForm').stop().slideToggle();
+});
+
+$('.searchForm i.xi-close').on('click', function(){
+    $('.searchForm').slideUp();
 });
 
 $('.main_slider').slick({
@@ -37,6 +53,26 @@ $('.pr_slider figure').eq(5).addClass('on');
 $('.pr_slider').on('afterChange',function(e,s,c){
     $('.pr_slider figure').eq(c+4).addClass('on').siblings().removeClass('on');
 });
+
+
+$('.familylink .link>a').on('click', function(e){
+    e.preventDefault(); //어떤 이벤트를 명시적으로 처리하지 않은 경우, 해당 이벤트에 대한 사용자 에이전트의 기본 동작을 실행하지 않도록 지정합니다.
+    $(this).prev().stop().slideToggle();
+    //stop(). 이벤트가 여러번 클릭해도 여러번 적용되지 않도록
+    $(this).find('i').toggleClass('on');
+    return false();
+});
+
+//totop
+$('#totop a').on('click',e=> {
+    e.preventDefault();
+    $('html,body').animate({scrollTop:0},1000, function(){
+        console.log('111'); //callback
+    });
+});
+
+let sTop = $('#solution').offset().top;
+
 
 //--------------------
 })
