7a7ecb73465e86ce33550bee1e11737899d0226b
ejin0517 <91935021+ejin0517@users.noreply.github.com>
2022-01-24 15:30:44 +0900

    Add files via upload

--- portfolio00/js/main.js
+++ portfolio00/js/main.js
@@ -4,7 +4,7 @@ $(function(){
 new fullpage('#fullpage', {
     anchors: ['main', 'project01', 'project02', 'project03', 'project04', 'project05', 'project06', 'profile'],
     keyboardScrolling: true,
-    responsiveWidth:769,
+    //responsiveWidth:769,
 
     afterLoad: function(origin, destination, direction){
       let txt = $('.section').eq(destination.index).find('strong').text();
